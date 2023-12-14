d537f268a751c916ec1ba2d91afb5f93a19201a9
fedeperin <federicoperin@outlook.com.ar>
2023-12-14 11:17:00 -0300

    Change the routes of all images

--- assets/data/es/books.es.js
+++ assets/data/es/books.es.js
@@ -6,7 +6,7 @@ export default [
         "releaseDate": "June 26, 1997",
         "description": "El día de su cumpleaños, Harry Potter descubre que es hijo de dos conocidos hechiceros, de los que ha heredado poderes mágicos. Debe asistir a una famosa escuela de magia y hechicería, donde entabla una amistad con dos jóvenes que se convertirán en sus compañeros de aventura. Durante su primer año en Hogwarts, descubre que un malévolo y poderoso mago llamado Voldemort está en busca de una piedra filosofal que alarga la vida de quien la posee.",
         "pages": 223,
-        "cover": "../images/covers/1.png"
+        "cover": "https://apipotter.vercel.app/images/covers/1.png"
     },
     {
         "number": 2,
@@ -15,7 +15,7 @@ export default [
         "releaseDate": "July 2, 1998",
         "description": "Harry Potter y los estudiantes de segundo año investigan una malévola amenaza para sus compañeros de clases de Hogwarts, una amenazante bestia que se esconde dentro del castillo.",
         "pages": 251,
-        "cover": "../images/covers/2.png"
+        "cover": "https://apipotter.vercel.app/images/covers/2.png"
     },
     {
         "number": 3,
@@ -24,7 +24,7 @@ export default [
         "releaseDate": "Jul 8, 1999",
         "description": "El tercer año de estudios de Harry en Hogwarts se ve amenazado por la fuga de Sirius Black de la prisión de Azkaban. Al parecer, se trata de un peligroso mago que fue cómplice de Lord Voldemort y que intentará vengarse de Harry Potter.",
         "pages": 317,
-        "cover": "../images/covers/3.png"
+        "cover": "https://apipotter.vercel.app/images/covers/3.png"
     },
     {
         "number": 4,
@@ -33,7 +33,7 @@ export default [
         "releaseDate": "Jul 8, 2000",
         "description": "Hogwarts se prepara para el Torneo de los Tres Magos, en el que competirán tres escuelas de hechicería. Para sorpresa de todos, Harry Potter es elegido para participar en la competencia, en la que deberá luchar contra dragones, internarse en el agua y enfrentarse a sus mayores miedos.",
         "pages": 636,
-        "cover": "../images/covers/4.png"
+        "cover": "https://apipotter.vercel.app/images/covers/4.png"
     },
     {
         "number": 5,
@@ -42,7 +42,7 @@ export default [
         "releaseDate": "Jun 21, 2003",
         "description": "En su quinto año en Hogwarts, Harry descubre que muchos integrantes de la comunidad de magos no conocen la verdad acerca de su encuentro con Lord Voldemort. Cornelius Fudge, ministro de magia, designa a Dolores Umbridge como maestra de defensa contra de las artes oscuras porque cree que el profesor Dumbledore planea apoderarse de su trabajo. Pero sus enseñanzas son inadecuadas, por lo que Harry prepara a los estudiantes para defender la escuela en contra del mal.",
         "pages": 766,
-        "cover": "../images/covers/5.png"
+        "cover": "https://apipotter.vercel.app/images/covers/5.png"
     },
     {
         "number": 6,
@@ -51,7 +51,7 @@ export default [
         "releaseDate": "Jul 16, 2005",
         "description": "Harry descubre un poderoso libro y, mientras trata de descubrir sus orígenes, colabora con Dumbledore en la búsqueda de una serie de objetos mágicos que ayudarán en la destrucción de Lord Voldemort.",
         "pages": 607,
-        "cover": "../images/covers/6.png"
+        "cover": "https://apipotter.vercel.app/images/covers/6.png"
     },
     {
         "number": 7,
@@ -60,7 +60,7 @@ export default [
         "releaseDate": "Jul 21, 2007",
         "description": "Harry, Ron y Hermione se van a una peligrosa misión para localizar y destruir el secreto de la inmortalidad y destrucción de Voldemort - los Horrocruces. Solos, sin la guía de sus profesores o la protección del Profesor Dumbledore, los tres amigos deben apoyarse el uno en el otro más que nunca. Pero hay Fuerzas Oscuras en medio que amenazan con separarlos. Harry Potter se está acercando cada vez más a la tarea para la que se ha preparado desde el primer día que pisó Hogwarts: la última batalla con Voldemort.",
         "pages": 607,
-        "cover": "../images/covers/7.png"
+        "cover": "https://apipotter.vercel.app/images/covers/7.png"
     },
     {
         "number": 8,
@@ -69,6 +69,6 @@ export default [
         "releaseDate": "Jul 30, 2016",
         "description": "El segundo hijo de Harry entró a Hogwarts, pero en Slytherin. Su relación con Harry cada vez es peor y se hizo muy amigo del hijo de Draco, Scorpius Malfoy que se dice que es el hijo de Lord Voldemort.",
         "pages": 336,
-        "cover": "../images/covers/8.png"
+        "cover": "https://apipotter.vercel.app/images/covers/8.png"
     }
 ]
