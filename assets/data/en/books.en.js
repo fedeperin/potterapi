952ee3dd1c9c920c66aa9948d9c49f09b71bd2d0
fedeperin <federicoperin@outlook.com.ar>
2023-12-15 16:48:15 -0300

    End adding urls

--- assets/data/en/books.en.js
+++ assets/data/en/books.en.js
@@ -15,7 +15,7 @@ export default [
         "releaseDate": "Jul 2, 1998",
         "description": "Harry Potter and the sophomores investigate a malevolent threat to their Hogwarts classmates, a menacing beast that hides within the castle.",
         "pages": 251,
-        "cover": "https://raw.githubusercontent.com/fedeperin/harry-potter-api/main/images/covers/2.png"
+        "cover": "https://raw.githubusercontent.com/fedeperin/potterapi/main/public/images/covers/2.png"
     },
     {
         "number": 3,
@@ -24,7 +24,7 @@ export default [
         "releseDay": "Jul 8, 1999",
         "description": "Harry's third year of studies at Hogwarts is threatened by Sirius Black's escape from Azkaban prison. Apparently, it is a dangerous wizard who was an accomplice of Lord Voldemort and who will try to take revenge on Harry Potter.",
         "pages": 317,
-        "cover": "https://raw.githubusercontent.com/fedeperin/harry-potter-api/main/images/covers/3.png"
+        "cover": "https://raw.githubusercontent.com/fedeperin/potterapi/main/public/images/covers/3.png"
     },
     {
         "number": 4,
@@ -33,7 +33,7 @@ export default [
         "releaseDate": "Jul 8, 2000",
         "description": "Hogwarts prepares for the Triwizard Tournament, in which three schools of wizardry will compete. To everyone's surprise, Harry Potter is chosen to participate in the competition, in which he must fight dragons, enter the water and face his greatest fears.",
         "pages": 636,
-        "cover": "https://raw.githubusercontent.com/fedeperin/harry-potter-api/main/images/covers/4.png"
+        "cover": "https://raw.githubusercontent.com/fedeperin/potterapi/main/public/images/covers/4.png"
     },
     {
         "number": 5,
@@ -42,7 +42,7 @@ export default [
         "releaseDate": "Jun 21, 2003",
         "description": "In his fifth year at Hogwarts, Harry discovers that many members of the wizarding community do not know the truth about his encounter with Lord Voldemort. Cornelius Fudge, Minister of Magic, appoints Dolores Umbridge as Defense Against the Dark Arts teacher because he believes that Professor Dumbledore plans to take over her job. But his teachings are inadequate, so Harry prepares the students to defend the school against evil.",
         "pages": 766,
-        "cover": "https://raw.githubusercontent.com/fedeperin/harry-potter-api/main/images/covers/5.png"
+        "cover": "https://raw.githubusercontent.com/fedeperin/potterapi/main/public/images/covers/5.png"
     },
     {
         "number": 6,
@@ -51,7 +51,7 @@ export default [
         "releaseDate": "Jul 16, 2005",
         "description": "Harry discovers a powerful book and, while trying to discover its origins, collaborates with Dumbledore in the search for a series of magical objects that will aid in the destruction of Lord Voldemort.",
         "pages": 607,
-        "cover": "https://raw.githubusercontent.com/fedeperin/harry-potter-api/main/images/covers/6.png"
+        "cover": "https://raw.githubusercontent.com/fedeperin/potterapi/main/public/images/covers/6.png"
     },
     {
         "number": 7,
@@ -60,7 +60,7 @@ export default [
         "releaseDate": "Jul 21, 2007",
         "description": "Harry, Ron and Hermione go on a dangerous mission to locate and destroy the secret of Voldemort's immortality and destruction - the Horcruces. Alone, without the guidance of their teachers or the protection of Professor Dumbledore, the three friends must lean on each other more than ever. But there are Dark Forces in between that threaten to tear them apart. Harry Potter is getting closer and closer to the task for which he has been preparing since the first day he set foot in Hogwarts: the last battle with Voldemort.",
         "pages": 607,
-        "cover": "https://raw.githubusercontent.com/fedeperin/harry-potter-api/main/images/covers/7.png"
+        "cover": "https://raw.githubusercontent.com/fedeperin/potterapi/main/public/images/covers/7.png"
     },
     {
         "number": 8,
@@ -69,6 +69,6 @@ export default [
         "releaseDate": "Jul 30, 2016",
         "description": "Harry's second son entered Hogwarts, but in Slytherin. His relationship with Harry is getting worse and he became close friends with Draco's son, Scorpius Malfoy who is said to be Lord Voldemort's son.",
         "pages": 336,
-        "cover": "https://raw.githubusercontent.com/fedeperin/harry-potter-api/main/images/covers/8.png"
+        "cover": "https://raw.githubusercontent.com/fedeperin/potterapi/main/public/images/covers/8.png"
     }
 ]
