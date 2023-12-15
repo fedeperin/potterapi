4b835bb18595ae9ae2b2f5d7592e8088f45a52c8
fedeperin <federicoperin@outlook.com.ar>
2023-12-15 14:04:51 -0300

    Take out configurations for vercel

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(cors())
 
 app.get('/:lang/:type', routeStructure)
 app.get('/:lang/:type/random', pickRandomItem)
 app.get('/', (req, res) => res.send('ApiPotter!'))
 
 app.listen(PORT, () => console.log(`Server running at port ${ PORT }`))
-
-export default app
