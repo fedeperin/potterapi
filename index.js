beaf293e7ef06319481607c305958d7d53a57fab
fedeperin <federicoperin@outlook.com.ar>
2023-12-31 01:30:41 -0300

    Implement Treblle

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+import 'dotenv/config'
 import express from 'express'
 import { routeStructure, pickRandomItem, pureLangRoute, homePage } from './assets/routes.js'
 import cors from 'cors'
+import treblle from '@treblle/express'
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(cors())
+app.use(treblle({
+    apiKey: process.env.TREBLLE_API_KEY,
+    projectId: process.env.TREBLLE_PROJECT_ID,
+    additionalFieldsToMask: []
+}))
 
 app.get('/:lang', pureLangRoute)
 app.get('/:lang/:type', routeStructure)
