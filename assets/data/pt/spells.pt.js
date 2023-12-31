789702f3cb408997ce86917e5f887cdc75c909b0
fedeperin <federicoperin2009@gmail.com>
2023-12-31 11:47:33 -0300

    feat(db): add the portuguese spells

---
+++ assets/data/pt/spells.pt.js
@@ -0,0 +1,290 @@
+export default [
+    {
+        "spell": "Accio",
+        "use": "Feitiço de invocação"
+    },
+    {
+        "spell": "Glisseo",
+        "use": "Transforma uma escada em um escorregador"
+    },
+    {
+        "spell": "Impervius",
+        "use": "Protege o lançador de uma variedade de substâncias, incluindo água e fogo"
+    },
+    {
+        "spell": "Wingardium Leviosa/Locomotor",
+        "use": "Levita objetos"
+    },
+    {
+        "spell": "Scourgify",
+        "use": "Limpa coisas"
+    },
+    {
+        "spell": "Portus",
+        "use": "Cria uma chave de portal"
+    },
+    {
+        "spell": "Orchideous",
+        "use": "Cresce flores na ponta da varinha"
+    },
+    {
+        "spell": "Lumos",
+        "use": "Faz a varinha emitir luz"
+    },
+    {
+        "spell": "Reparo",
+        "use": "Repara objetos quebrados"
+    },
+    {
+        "spell": "Reducio",
+        "use": "Encolhe objetos"
+    },
+    {
+        "spell": "Pack",
+        "use": "Embala baús e bagagem"
+    },
+    {
+        "spell": "Riddikulus",
+        "use": "Transforma Boggarts em uma forma humorística"
+    },
+    {
+        "spell": "Protego",
+        "use": "Protege o lançador de maldições"
+    },
+    {
+        "spell": "Muffliato",
+        "use": "Impede que outras pessoas próximas ouçam conversas"
+    },
+    {
+        "spell": "Silencio/Langlock",
+        "use": "Impede alguém de falar (colocando a língua no céu da boca)"
+    },
+    {
+        "spell": "Expelliarmus",
+        "use": "Feitiço de desarmamento"
+    },
+    {
+        "spell": "Oblivate",
+        "use": "Erases memories"
+    },
+    {
+        "spell": "Episkey",
+        "use": "Cura ferimentos leves"
+    },
+    {
+        "spell": "Tergeo",
+        "use": "Limpa superfícies"
+    },
+    {
+        "spell": "Relashio",
+        "use": "Força o alvo a largar tudo o que está segurando"
+    },
+    {
+        "spell": "Confundo",
+        "use": "Causa confusões e torna outras pessoas facilmente suscetíveis à influência"
+    },
+    {
+        "spell": "Expecto Patronum",
+        "use": "Amuleto Patrono"
+    },
+    {
+        "spell": "Ferula",
+        "use": "Conjura bandagens"
+    },
+    {
+        "spell": "Evanesco",
+        "use": "Feitiço de desaparecimento"
+    },
+    {
+        "spell": "Alohomora",
+        "use": "Abre fechaduras"
+    },
+    {
+        "spell": "Nox",
+        "use": "Desliga o Lumus"
+    },
+    {
+        "spell": "Quietus",
+        "use": "Acalma a voz magicamente amplificada"
+    },
+    {
+        "spell": "Incendio",
+        "use": "Cria fogo"
+    },
+    {
+        "spell": "Homenum Revelio",
+        "use": "Revelar pessoas próximas"
+    },
+    {
+        "spell": "Prior Incantato",
+        "use": "Revela o último feitiço lançado"
+    },
+    {
+        "spell": "Finite Incantatem",
+        "use": "Nega os efeitos de muitos feitiços"
+    },
+    {
+        "spell": "Erecto",
+        "use": "Ergue tendas ou outras estruturas"
+    },
+    {
+        "spell": "Diffindo",
+        "use": "Corta ou rasga material"
+    },
+    {
+        "spell": "Stupefy",
+        "use": "Atordoa o alvo"
+    },
+    {
+        "spell": "Repelo Muggletum",
+        "use": "Repele trouxas"
+    },
+    {
+        "spell": "Avada Kedavra",
+        "use": "A maldição da morte"
+    },
+    {
+        "spell": "Aguamenti",
+        "use": "Atire água da varinha"
+    },
+    {
+        "spell": "Geminio",
+        "use": "Cria duplicatas temporárias e sem valor de qualquer objeto"
+    },
+    {
+        "spell": "Locomotor Mortis",
+        "use": "Maldição do leglock"
+    },
+    {
+        "spell": "Anapneo",
+        "use": "Limpa as vias respiratórias de alguém"
+    },
+    {
+        "spell": "Reducto",
+        "use": "Explode objeto"
+    },
+    {
+        "spell": "Obscuro",
+        "use": "Alvo de vendas"
+    },
+    {
+        "spell": "Impedimenta",
+        "use": "Congela alguém avançando em sua direção"
+    },
+    {
+        "spell": "Cave Inimicum/Protego Totalum",
+        "use": "Fortalece as defesas de uma área"
+    },
+    {
+        "spell": "Meteolojinx Recanto",
+        "use": "Acaba com os efeitos dos feitiços climáticos"
+    },
+    {
+        "spell": "Specialis Revelio",
+        "use": "Revela propriedades mágicas ocultas em um objeto"
+    },
+    {
+        "spell": "Descendo",
+        "use": "Move objetos para baixo"
+    },
+    {
+        "spell": "Defodio",
+        "use": "Esculpe pedra e aço"
+    },
+    {
+        "spell": "Aparecium",
+        "use": "Faça aparecer tinta invisível"
+    },
+    {
+        "spell": "Piertotum Locomotor",
+        "use": "Anima estátuas"
+    },
+    {
+        "spell": "Imperio",
+        "use": "Faz o alvo obedecer a todos os comandos"
+    },
+    {
+        "spell": "Fidelius Charm",
+        "use": "Liga um segredo à alma de um Guardião do Segredo"
+    },
+    {
+        "spell": "Avis/Oppugno",
+        "use": "Atira bandos de pássaros com a varinha"
+    },
+    {
+        "spell": "Expulso",
+        "use": "Explode objetos"
+    },
+    {
+        "spell": "Legilimens",
+        "use": "Revela memórias e pensamentos do alvo"
+    },
+    {
+        "spell": "Duro",
+        "use": "Endurece objetos"
+    },
+    {
+        "spell": "Sonorus",
+        "use": "Amplifica a voz"
+    },
+    {
+        "spell": "Deprimo",
+        "use": "Cria grande pressão descendente"
+    },
+    {
+        "spell": "Levicorpus",
+        "use": "Levita o alvo pelo tornozelo"
+    },
+    {
+        "spell": "Liberacorpus",
+        "use": "Reduz o alvo do levicorpus"
+    },
+    {
+        "spell": "Mobilicorpus",
+        "use": "Move corpos"
+    },
+    {
+        "spell": "Confringo",
+        "use": "Explode objetos em chamas"
+    },
+    {
+        "spell": "Densaugeo",
+        "use": "Faz os dentes crescerem rapidamente"
+    },
+    {
+        "spell": "Incarcarous",
+        "use": "Amarra alguém com cordas"
+    },
+    {
+        "spell": "Deletrius",
+        "use": "Ignora os efeitos do Prior Incantato"
+    },
+    {
+        "spell": "Rictusempra",
+        "use": "Feitiço de cócegas"
+    },
+    {
+        "spell": "Petrificus Totalus",
+        "use": "Torna o alvo completamente imóvel"
+    },
+    {
+        "spell": "Fiendfyre Curse",
+        "use": "Faz fogo amaldiçoado"
+    },
+    {
+        "spell": "Tarantallegra",
+        "use": "Força o alvo a dançar"
+    },
+    {
+        "spell": "Morsmordre",
+        "use": "Conjura a Marca Negra"
+    },
+    {
+        "spell": "Sectumsempra",
+        "use": "Causa lacerações graves"
+    },
+    {
+        "spell": "Crucio",
+        "use": "Causa imensa dor"
+    }
+]
