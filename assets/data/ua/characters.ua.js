5f8567d91a8276695d7d2cb3bcff8ce983acc113
romsendz <sendzyuk.r@gmail.com>
2025-01-13 18:35:21 +0100

    added base files for ukrainian

---
+++ assets/data/ua/characters.ua.js
@@ -0,0 +1,194 @@
+export default [
+    {
+        "fullName": "Гаррі Джеймс Поттер",
+        "nickname": "Гаррі",
+        "hogwartsHouse": "Ґрифіндор",
+        "children": [
+            "Джеймс Сіріус Поттер",
+            "Албус Северус Поттер",
+            "Лілі Луна Поттер"
+        ]
+    },
+    {
+        "fullName": "Герміона Джин Ґрейнджер",
+        "nickname": "Герміона",
+        "hogwartsHouse": "Ґрифіндор",
+        "children": [
+            "Роуз Ґрейнджер-Візлі",
+            "Гюґо Ґрейнджер-Візлі"
+        ]
+    },
+    {
+        "fullName": "Рональд Біліус Візлі",
+        "nickname": "Рон",
+        "hogwartsHouse": "Ґрифіндор",
+        "children": [
+            "Роуз Ґрейнджер-Візлі",
+            "Гюґо Ґрейнджер-Візлі"
+        ]
+    },
+    {
+        "fullName": "Фредерік Ґідеон Візлі",
+        "nickname": "Фред",
+        "hogwartsHouse": "Ґрифіндор",
+        "children": []
+    },
+    {
+        "fullName": "Джордж Візлі",
+        "nickname": "Джордж",
+        "hogwartsHouse": "Ґрифіндор",
+        "children": []
+    },
+    {
+        "fullName": "Білл Візлі",
+        "nickname": "Білл",
+        "hogwartsHouse": "Ґрифіндор",
+        "children": []
+    },
+    {
+        "fullName": "Персі Візлі",
+        "nickname": "Персі",
+        "hogwartsHouse": "Ґрифіндор",
+        "children": []
+    },
+    {
+       "fullName": "Чарлі Візлі",
+        "nickname": "Чарлі",
+        "hogwartsHouse": "Ґрифіндор",
+        "children": []
+    },
+    {
+        "fullName": "Джині Візлі",
+        "nickname": "Джині",
+        "hogwartsHouse": "Ґрифіндор",
+        "children": [
+            "Джеймс Сіріус Поттер",
+            "Албус Северус Поттер",
+            "Лілі Луна Поттер"
+        ]
+    },
+    {
+        "fullName": "Моллі Візлі",
+        "nickname": "Моллі",
+        "hogwartsHouse": "Ґрифіндор",
+        "children": [
+            "Рон Візлі",
+            "Фред Візлі",
+            "Джордж Візлі",
+            "Білл Візлі",
+            "Персі Візлі",
+            "Чарлі Візлі",
+            "Джині Візлі"
+        ]
+    },
+    {
+        "fullName": "Артур Візлі",
+        "nickname": "Артур",
+        "hogwartsHouse": "Ґрифіндор",
+        "children": [
+            "Рон Візлі",
+            "Фред Візлі",
+            "Джордж Візлі",
+            "Білл Візлі",
+            "Персі Візлі",
+            "Чарлі Візлі",
+            "Джині Візлі"
+        ]
+    },
+    {
+        "fullName": "Невіл Лонґботом",
+        "nickname": "Невіл",
+        "hogwartsHouse": "Ґрифіндор",
+        "children": []
+    },
+    {
+        "fullName": "Луна Лавґуд",
+        "nickname": "Луна",
+        "hogwartsHouse": "Рейвенклов",
+        "children": []
+    },
+    {
+        "fullName": "Драко Мелфой",
+        "nickname": "Драко",
+        "hogwartsHouse": "Слизерин",
+        "children": [
+            "Скорпіус Мелфой"
+        ]
+    },
+    {
+        "fullName": "Албус Персіваль Вульфрик Браян Дамблдор",
+        "nickname": "Дамблдор",
+        "hogwartsHouse": "Ґрифіндор",
+        "children": []
+    },
+    {
+        "fullName": "Мінерва Макґонеґел",
+        "nickname": "Мінерва",
+        "hogwartsHouse": "Ґрифіндор",
+        "children": []
+    },
+    {
+        "fullName": "Ремус Люпин",
+        "nickname": "Люпин",
+        "hogwartsHouse": "Ґрифіндор",
+        "children": [
+            "Тед Люпин"
+        ]
+    },
+    {
+        "fullName": "Рубеус Геґрід",
+        "nickname": "Геґрід",
+        "hogwartsHouse": "Ґрифіндор",
+        "children": []
+    },
+    {
+        "fullName": "Сіріус Блек",
+        "nickname": "Сіріус",
+        "hogwartsHouse": "Ґрифіндор",
+        "children": []
+    },
+    {
+        "fullName": "Северус Снейп",
+        "nickname": "Снейп",
+        "hogwartsHouse": "Слизерин",
+        "children": []
+    },
+    {
+        "fullName": "Белатриса Лестранж",
+        "nickname": "Белла",
+        "hogwartsHouse": "Слизерин",
+        "children": [
+            "Дельфі"
+        ]
+    },
+    {
+        "fullName": "Лорд Волдеморт",
+        "nickname": "Волдеморт",
+        "hogwartsHouse": "Слизерин",
+        "children": [
+            "Дельфі"
+        ]
+    },
+    {
+        "fullName": "Седрик Діґорі",
+        "nickname": "Седрик",
+        "hogwartsHouse": "Гафелпаф",
+        "children": []
+    },
+    {
+        "fullName": "Німфадора Тонкс",
+        "nickname": "Тонкс",
+        "hogwartsHouse": "Гафелпаф",
+        "children": [
+            "Тед Люпин"
+        ]
+    },
+    {
+        "fullName": "Джеймс Поттер",
+        "nickname": "Джеймс",
+        "hogwartsHouse": "Ґрифіндор",
+        "children": [
+            "Гаррі Поттер"
+        ]
+    }
+]
